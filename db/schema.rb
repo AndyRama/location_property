3be950b8c0ba659109e027a4785eb65a6896e65f
AndyRama <andyramaroson@gmail.com>
2021-04-03 23:05:24 +0200

    model property

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_204509) do
+ActiveRecord::Schema.define(version: 2021_04_03_210359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "photo"
+    t.string "status"
+    t.integer "price"
+    t.integer "rooms"
+    t.integer "bathrooms"
+    t.integer "parking_spaces"
+    t.boolean "for_sale"
+    t.datetime "available_date"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

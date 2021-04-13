fd1b6e1d5d0f2ddb3e03da79636aaa3ef1e2fbfe
AndyRama <andyramaroson@gmail.com>
2021-04-13 14:13:02 +0200

    fixe property-thumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
+
   def profile_picture current_user, width = 100
     thumb = current_user.images.present? ? current_user.images.thumb.url : "placeholder.png"
+    # image_tag thumb, class: "profile_pic img-circle ml-4"
     image_tag "id.png", width: width, class: "profile_pic img-circle ml-4"
   end
-
 end
- 

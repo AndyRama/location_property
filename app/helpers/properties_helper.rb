3969d28d8a3252a7a74bbe8ab6642f77fc848fe4
AndyRama <andyramaroson@gmail.com>
2021-04-13 18:53:46 +0200

    add image

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -5,6 +5,10 @@ module PropertiesHelper
     image_tag img, class: "property-thumb"
   end
 
+  def property_thumbnail_url(property)
+    property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+  end
+
   def property_photo(property)
     img = property.photo.present? ? property.photo.url : "placeholder.png"
     image_tag img, class: "property-photo"

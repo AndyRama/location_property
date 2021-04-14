ca01831ea5730ae96383ea677d9287615c739b5b
AndyRama <andyramaroson@gmail.com>
2021-04-14 23:57:01 +0200

    add modal for email btn

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -1,17 +1,24 @@
 $(function(){ 
+  // for reveal btn phone/number 
   $("#show-tel").on("click", function(){
     const $this = $(this);
     $this.find("span").text( $this.data("number") );
   });
 
+  // for reveal btn showmore/description 
   $("#toggleDetails").on("click", function(){
     const details = $("#prop-details");
     details.toggleClass("open");
     if(details.hasClass("open")){
-    // console.log('class found')
       $(this).text("Show less");
     } else {
       $(this).text("Show More");
     }
   }); 
+  
+  // for modal to btn email
+  $('#contact-modal').on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget);
+    const modal = $(this);
+  });
 });

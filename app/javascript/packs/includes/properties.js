141f9f958df107deebef5753fedb92e2dcbfc472
AndyRama <andyramaroson@gmail.com>
2021-04-14 22:20:07 +0200

    add map

--- app/javascript/packs/includes/properties.js
+++ app/javascript/packs/includes/properties.js
@@ -1,8 +1,12 @@
 $(function(){ 
   $("#toggleDetails").on("click", function(){
     var details = $("#prop-details");
-    
-    // console.log('clicked')
     details.toggleClass("open");
+    if(details.hasClass("open")){
+    // console.log('class found')
+      $(this).text("Show less");
+    } else {
+      $(this).text("Show More");
+    }
   }); 
 });

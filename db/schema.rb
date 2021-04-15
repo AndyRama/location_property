69ee77daf20d9100b0755e258e56cc09609d076a
AndyRama <andyramaroson@gmail.com>
2021-04-15 23:28:46 +0200

    add details to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_172441) do
+ActiveRecord::Schema.define(version: 2021_04_15_211858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_172441) do
     t.string "images"
     t.string "company"
     t.string "number"
+    t.text "details"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

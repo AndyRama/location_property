853a0b4718a8d0c892eb6262564ca5d43ec271fc
AndyRama <andyramaroson@gmail.com>
2021-04-15 17:31:43 +0200

    add admin user

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,8 +1,8 @@
 class PublicController < ApplicationController
   def main
     if user_signed_in?
-      # path = current_user.admin? ? user_path : properties_path
-      redirect_to dashboard_path, flash: { success: "Successfully signed in. Welcome to Location Property" } and return
+      path = current_user.admin? ? users_path : dashboard_path
+      redirect_to path, flash: { success: "Successfully signed in. Welcome to Location Property" } and return
     end
 
     @properties = Property.latest

961e0327d0bb623b27932abee7b2a9581a06f20c
AndyRama <andyramaroson@gmail.com>
2021-04-16 18:27:47 +0200

    add cover_image to property

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,5 +1,5 @@
 module ProfileHelper
   def cover_image_url user
-    user.cover_image.attached? ? url_for(user.cover_image) : 'placeholder.png'
+    user.cover_image.present? ? user.cover_image_url : asset_url("placeholder.png")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_one_attached :image
   has_one_attached :cover_image
   
-  mount_uploader :photos, ProfilePictureUploader
-  serialize :photos, JSON # If you use SQLite, add this line.
+  mount_uploader :image, ProfilePictureUploader
+  mount_uploader :cover_image, ProfilePictureUploader
 
   def full_name
     "#{first_name} #{last_name}"

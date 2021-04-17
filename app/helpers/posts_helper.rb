b0ea63da94f0241651d40d592cc1cc4869f18f49
AndyRama <andyramaroson@gmail.com>
2021-04-17 14:46:21 +0200

    change main page public

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,4 @@
 module PostsHelper
-  def post_thumbnail(post)
-    img = post.image.present? ? post.image.thumb.url : "placeholder.png"
-    image_tag img, class: "property-thumb"
-  end
-
   def post_thumbnail_url(post)
     post.image.present? ? post.image.thumb.url : "placeholder.png"
   end

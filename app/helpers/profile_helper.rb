369e79f7e7dbca32bf9eeccb02376604c86ba492
AndyRama <andyramaroson@gmail.com>
2021-04-18 17:07:23 +0200

    fix bug heroku

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -2,4 +2,8 @@ module ProfileHelper
   def cover_image_url user
     user.cover_image.present? ? user.cover_image_url : asset_url("placeholder.png")
   end
+
+  def property_photo_url(property)
+    property.photo.present? ? property.photo.url : asset_url("placeholder.png")
+  end
 end

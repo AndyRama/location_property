a60a885a1e0e5987b9096ab2fe0a77447f673641
AndyRama <andyramaroson@gmail.com>
2021-04-18 21:08:34 +0200

    heroku pb route

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -3,7 +3,7 @@ module ProfileHelper
     user.cover_image.present? ? user.cover_image_url : asset_url("placeholder.png")
   end
 
-  def property_photo_url(property)
-    property.photo.present? ? property.photo.url : asset_url("placeholder.png")
-  end
+  # def property_photo_url(property)
+  #   property.photo.present? ? property.photo.url : asset_url("placeholder.png")
+  # end
 end

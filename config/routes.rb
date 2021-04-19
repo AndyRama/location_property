5ad31d0f39602153447ac1d9382c90c447e7d7eb
AndyRama <andyramaroson@gmail.com>
2021-04-19 17:43:35 +0200

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'public#main'
-  resources :public  
-
+  # resources :public  
   resources :posts
   resources :properties
- 
+
+  get "/public" => "public#main", as: :public
 
   get "/blog" => "posts#latest", as: :blog
 
@@ -16,4 +15,6 @@ Rails.application.routes.draw do
   get "/profile/:id" => 'dashboard#profile', as: :profile
 
   post "/agent/message" =>  "properties#email_agent", as: :email_agent
+
+  root to: 'public#main'
 end

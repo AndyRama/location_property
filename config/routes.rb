c9d3cc8eeccbc6c01758236cd57d81dcb56f52a1
AndyRama <andyramaroson@gmail.com>
2021-04-21 11:28:39 +0200

    btn profile acces restaured

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,17 @@ Rails.application.routes.draw do
 
   get "/blog" => "posts#latest", as: :blog
 
+  # get "/For_Rent" => "property#latest", as: :for_rent
+  # get "/For_Sale" => "property#latest", as: :for_sale
+  # get "Advertise" => "#", as: :advertise
+  # get "/Contact" => "#", as: :contact
+
+  get "/blog" => "posts#latest", as: :blog
+
   get "/users" => "admin#users", as: :users
 
   get "/dashboard" => 'dashboard#index', as: :dashboard
+
   get "/profile/:id" => 'dashboard#profile', as: :profile
 
   post "/agent/message" =>  "properties#email_agent", as: :email_agent

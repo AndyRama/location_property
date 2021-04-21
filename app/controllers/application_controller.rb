fd1fdd472d354fe9e203b30441c8745eb9415d0d
AndyRama <andyramaroson@gmail.com>
2021-04-21 23:38:25 +0200

    issue photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     @show_sidebar = true if user_signed_in?
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name url number image image_cache company])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name url number image image_cache cover_image cover_image_cache company])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name url cover_image cover_image_cache photo photo_cache company number details])

abcb2df3bc54314b6a031ce338774c95af9231f8
AndyRama <andyramaroson@gmail.com>
2021-04-23 17:07:05 +0200

    path heroku

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,10 +1,10 @@
 class PublicController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:main]
-  # before_action :authenticate_user!, except: %i[home]
+  before_action :authenticate_user!, except: %i[home]
 
   def main
     if user_signed_in?
-      path = current_user.admin? ? users_path : dashboard_path
+      path = current_user.admin? ? users_path : properties_path
       redirect_to path, flash: { success: "Successfully signed in. Welcome to Location Property" } and return
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ print " - "
 print " Création echouée: 'X'"
 puts "\n\n Création de 1 Utilisateur"
 
-andy = User.new(email:"andy@gmail.com", encrypted_password:"52KgrDYiewMgySx", first_name:"andy", last_name:"rama")
+User2 = User.new(email:"andy@gmail.com", encrypted_password:"52KgrDYiewMgySx", first_name:"andy", last_name:"rama")
 if andy.save
  print('.')
 end

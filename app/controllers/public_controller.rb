1bc13b3f31357d0a20e23a6ab175f6b58bf9cdbb
AndyRama <andyramaroson@gmail.com>
2021-04-23 13:43:45 +0200

    skip login

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,5 @@
 class PublicController < ApplicationController
-  before_action :authenticate_user!, except: %i[main]
+  # before_action :authenticate_user!, except: %i[main]
 
   def main
     if user_signed_in?

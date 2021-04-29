5af8e9e6b49a1e458f84505db799e99151327968
AndyRama <andyramaroson@gmail.com>
2021-04-29 16:17:34 +0200

    test

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,10 +3,10 @@ class PublicController < ApplicationController
   # before_action :authenticate_user!, except: %i[home]
 
   def main
-    if user_signed_in?
-      path = current_user.admin? ? users_path : properties_path
-      redirect_to path, flash: { success: "Successfully signed in. Welcome to Location Property" } and return
-    end
+    # if user_signed_in?
+    #   path = current_user.admin? ? users_path : properties_path
+    #   redirect_to path, flash: { success: "Successfully signed in. Welcome to Location Property" } and return
+    # end
 
     @properties = Property.latest
     @posts = Post.latest
